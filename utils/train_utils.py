d6bd81e22ef85664bc5600437be005ea5f312f69
Cameron Laedtke <cameronlaedtke@MacBook-Pro.local>
2021-08-21 18:07:09 -0500

    initial commit

---
+++ utils/train_utils.py
@@ -0,0 +1,240 @@
+
+
+
+
+
+class CrossEntropy(nn.Module):
+    def __init__(self, ignore_label=-1, weight=None):
+        super(CrossEntropy, self).__init__()
+        self.ignore_label = ignore_label
+        self.criterion = nn.CrossEntropyLoss(weight=weight,ignore_index=ignore_label)
+
+    def _forward(self, score, target):
+        ph, pw = score.size(2), score.size(3)
+        h, w = target.size(1), target.size(2)
+        if ph != h or pw != w:
+            score = F.interpolate(input=score, size=(h, w), mode='bilinear', align_corners=True)
+
+        loss = self.criterion(score, target)
+
+        return loss
+
+    def forward(self, score, target):
+
+
+        score = [score]
+
+        weights = [1]
+        assert len(weights) == len(score)
+
+        return sum([w * self._forward(x, target) for (w, x) in zip(weights, score)])
+    
+    
+    
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.initialized = False
+        self.val = None
+        self.avg = None
+        self.sum = None
+        self.count = None
+
+    def initialize(self, val, weight):
+        self.val = val
+        self.avg = val
+        self.sum = val * weight
+        self.count = weight
+        self.initialized = True
+
+    def update(self, val, weight=1):
+        if not self.initialized:
+            self.initialize(val, weight)
+        else:
+            self.add(val, weight)
+
+    def add(self, val, weight):
+        self.val = val
+        self.sum += val * weight
+        self.count += weight
+        self.avg = self.sum / self.count
+
+    def value(self):
+        return self.val
+
+    def average(self):
+        return self.avg
+    
+    
+def create_logger(cfg, cfg_name, phase='train'):
+    root_output_dir = Path(cfg.OUTPUT_DIR)
+    # set up logger
+    if not root_output_dir.exists():
+        print('=> creating {}'.format(root_output_dir))
+        root_output_dir.mkdir()
+
+    dataset = cfg.DATASET.DATASET
+    model = cfg.MODEL.NAME
+    cfg_name = os.path.basename(cfg_name).split('.')[0]
+
+    final_output_dir = root_output_dir / dataset / cfg_name
+
+    print('=> creating {}'.format(final_output_dir))
+    final_output_dir.mkdir(parents=True, exist_ok=True)
+
+    time_str = time.strftime('%Y-%m-%d-%H-%M')
+    log_file = '{}_{}_{}.log'.format(cfg_name, time_str, phase)
+    final_log_file = final_output_dir / log_file
+    head = '%(asctime)-15s %(message)s'
+    logging.basicConfig(filename=str(final_log_file),
+                        format=head)
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    console = logging.StreamHandler()
+    logging.getLogger('').addHandler(console)
+
+    tensorboard_log_dir = Path(cfg.LOG_DIR) / dataset / model / (cfg_name + '_' + time_str)
+    print('=> creating {}'.format(tensorboard_log_dir))
+    tensorboard_log_dir.mkdir(parents=True, exist_ok=True)
+
+    return logger, str(final_output_dir), str(tensorboard_log_dir)
+
+    
+def get_confusion_matrix(label, pred, size, num_class, ignore=-1):
+    """
+    Calcute the confusion matrix by given label and pred
+    """
+    output = pred.cpu().numpy().transpose(0, 2, 3, 1)
+    seg_pred = np.asarray(np.argmax(output, axis=3), dtype=np.uint8)
+    seg_gt = np.asarray(
+    label.cpu().numpy()[:, :size[-2], :size[-1]], dtype=np.int)
+
+    ignore_index = seg_gt != ignore
+    seg_gt = seg_gt[ignore_index]
+    seg_pred = seg_pred[ignore_index]
+
+    index = (seg_gt * num_class + seg_pred).astype('int32')
+    label_count = np.bincount(index)
+    confusion_matrix = np.zeros((num_class, num_class))
+
+    for i_label in range(num_class):
+        for i_pred in range(num_class):
+            cur_index = i_label * num_class + i_pred
+            if cur_index < len(label_count):
+                confusion_matrix[i_label, i_pred] = label_count[cur_index]
+    return confusion_matrix
+
+
+def adjust_learning_rate(optimizer, base_lr, max_iters, cur_iters, power=0.9, nbb_mult=10):
+    lr = base_lr*((1-float(cur_iters)/max_iters)**(power))
+    optimizer.param_groups[0]['lr'] = lr
+    if len(optimizer.param_groups) == 2:
+        optimizer.param_groups[1]['lr'] = lr * nbb_mult
+    return lr
+
+
+
+def train(config, epoch, num_epoch, epoch_iters, base_lr, num_iters, trainloader, optimizer, model, writer_dict):
+    # Training
+    model.train()
+
+    batch_time = AverageMeter()
+    ave_loss = AverageMeter()
+    tic = time.time()
+    cur_iters = epoch*epoch_iters
+    writer = writer_dict['writer']
+    global_steps = writer_dict['train_global_steps']
+
+    for i_iter, batch in enumerate(trainloader, 0):
+        images, labels, _, _ = batch
+        images = images.cuda()
+        labels = labels.long().cuda()
+
+        losses, _ = model(images, labels)
+        loss = losses.mean()
+
+        reduced_loss = loss
+
+        model.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - tic)
+        tic = time.time()
+
+        # update average loss
+        ave_loss.update(reduced_loss.item())
+
+        lr = adjust_learning_rate(optimizer, base_lr, num_iters, i_iter+cur_iters)
+
+        if i_iter % 20 == 0:
+            msg = 'Epoch: [{}/{}] Iter:[{}/{}], Time: {:.2f}, lr: {}, Loss: {:.6f}' .format(
+                epoch, num_epoch, i_iter, epoch_iters, batch_time.average(), 
+                [x['lr'] for x in optimizer.param_groups], ave_loss.average())
+            logging.info(msg)
+
+    writer.add_scalar('train_loss', ave_loss.average(), global_steps)
+    writer_dict['train_global_steps'] = global_steps + 1
+    
+    
+def validate(config, testloader, model, writer_dict):
+    model.eval()
+    ave_loss = AverageMeter()
+    nums = config.MODEL.NUM_OUTPUTS
+    confusion_matrix = np.zeros((config.DATASET.NUM_CLASSES, config.DATASET.NUM_CLASSES, nums))
+    with torch.no_grad():
+        for idx, batch in enumerate(testloader):
+            image, label, _, _ = batch
+            size = label.size()
+            image = image.cuda()
+            label = label.long().cuda()
+
+            losses, pred = model(image, label)
+            if not isinstance(pred, (list, tuple)):
+                pred = [pred]
+            for i, x in enumerate(pred):
+                x = F.interpolate(
+                    input=x, size=size[-2:],
+                    mode='bilinear', align_corners=config.MODEL.ALIGN_CORNERS
+                )
+
+                confusion_matrix[..., i] += get_confusion_matrix(
+                    label,
+                    x,
+                    size,
+                    config.DATASET.NUM_CLASSES,
+                    config.TRAIN.IGNORE_LABEL
+                )
+
+            if idx % 10 == 0:
+                print(idx)
+
+            loss = losses.mean()
+            if dist.is_distributed():
+                reduced_loss = reduce_tensor(loss)
+            else:
+                reduced_loss = loss
+            ave_loss.update(reduced_loss.item())
+
+    if dist.is_distributed():
+        confusion_matrix = torch.from_numpy(confusion_matrix).cuda()
+        reduced_confusion_matrix = reduce_tensor(confusion_matrix)
+        confusion_matrix = reduced_confusion_matrix.cpu().numpy()
+
+    for i in range(nums):
+        pos = confusion_matrix[..., i].sum(1)
+        res = confusion_matrix[..., i].sum(0)
+        tp = np.diag(confusion_matrix[..., i])
+        IoU_array = (tp / np.maximum(1.0, pos + res - tp))
+        mean_IoU = IoU_array.mean()
+        if dist.get_rank() <= 0:
+            logging.info('{} {} {}'.format(i, IoU_array, mean_IoU))
+
+    writer = writer_dict['writer']
+    global_steps = writer_dict['valid_global_steps']
+    writer.add_scalar('valid_loss', ave_loss.average(), global_steps)
+    writer.add_scalar('valid_mIoU', mean_IoU, global_steps)
+    writer_dict['valid_global_steps'] = global_steps + 1
+    return ave_loss.average(), mean_IoU, IoU_array
